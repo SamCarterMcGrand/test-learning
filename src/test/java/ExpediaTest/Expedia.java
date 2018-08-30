c0d2da4316cc89e705d4b2283292cafaef1a32e0
samuelmcgrand <samuel.mcgrand@clearscore.com>
2018-08-30 12:40:34 +0100

    edit expedia test to include assertions

--- src/test/java/ExpediaTest/Expedia.java
+++ src/test/java/ExpediaTest/Expedia.java
@@ -1,4 +1,5 @@
 package ExpediaTest;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
@@ -12,7 +13,8 @@ public class Expedia {
 
     WebDriver driver;
     String browserType = "chrome";
-    String city = "Chicago, Illinois";
+    String city = "New York";
+    String cityAndState = "Chicago, Illinois";
     String checkIn = "10/10/2018";
     String checkOut = "10/17/2018";
     String numOfGuests = "2";
@@ -23,17 +25,31 @@ public class Expedia {
 
         // 1. Search
         driver.findElement(By.xpath("//*[@id=\"tab-hotel-tab-hp\"]")).click();
-        driver.findElement(By.id("hotel-destination-hp-hotel")).sendKeys(city);
+        driver.findElement(By.id("hotel-destination-hp-hotel")).sendKeys(cityAndState);
         driver.findElement(By.id("hotel-checkin-hp-hotel")).sendKeys(checkIn);
         driver.findElement(By.id("hotel-checkout-hp-hotel")).sendKeys(checkOut);
         driver.findElement(By.xpath("//*[@id=\"traveler-selector-hp-hotel\"]/div/ul/li/button")).click();
         driver.findElement(By.xpath("//*[@id=\"traveler-selector-hp-hotel\"]/div/ul/li/div/div/div/div[2]/div[4]/button")).click();
         // driver.findElement(By.xpath("//*[@id=\"hotel-business-travel-radio-no-hp-hotel\"]")).click();
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
+        driver.findElement(By.cssSelector("button[type='submit][class='btn-primary btn-action  gcw-submit']")).click();
+        String displayResults = driver.findElement(By.xpath("//*[@id=\"hotelResultTitle\"]/h1")).getText();
+
+        boolean dispResultsTruth = true;
+
+        if (displayResults.contains(city)) {
+            dispResultsTruth = true;
+        } else if (!displayResults.contains(city)) {
+            dispResultsTruth = false;
+        }
+        System.out.println(displayResults);
+
+
+        Assert.assertTrue(dispResultsTruth);
 
         // 2. Modify search results page by giving criteria
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.cssSelector("input[name='star'][id='star4']")).click();
+        driver.findElement(By.xpath("//*[@id=\"resultsContainer\"]/section/article[3]/div[2]/div/a")).click();
 
         // 3. Analyze results and make selection
 
